818cc91b950208e2f9dc92d864572698ec1ce3d1
Dhwani1401 <78218424+Dhwani1401@users.noreply.github.com>
2021-04-03 01:06:23 +0530

    3.04.21

--- popup.js
+++ popup.js
@@ -1,14 +1,57 @@
-//This section of code is to allow user to add tasks//
+
 document.getElementById("button1").addEventListener("click", myFunction);
- function myFunction(){
-  var li = document.createElement("li");
+
+let tasks = [];
+
+function myFunction() {
   var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue === "") {
     alert("entry is compulsory!");
   } else {
-    document.getElementById("myOL").appendChild(li);
+    tasks.push(inputValue);
+    clearList();
+    addTasksToList();
   }
   document.getElementById("myInput").value = "";
+}
+
+function clearList() {
+  document.getElementById("myOL").innerHTML = "";
+}
+
+function addTasksToList() {
+  let index = 0;
+  tasks.forEach((task) => {
+    var li = document.createElement("li");
+    var label = document.createElement("label");
+    var button = document.createElement("button");
+    button.setAttribute("id", "index");
+    var deleteText = document.createTextNode("X");
+    button.appendChild(deleteText);
+    var t = document.createTextNode(task);
+
+    label.appendChild(t);
+    li.appendChild(label);
+    li.appendChild(button);
+    document.getElementById("myOL").appendChild(li);
+
+    button.addEventListener("click", deleteTask);
+    index += 1;
+  });
+}
+
+function deleteTask(event) {
+  tasks.splice(event.target.id, 1);
+  clearList();
+  addTasksToList();
+}
+document.getElementById("button2").addEventListener("click",savelist);
+function savelist(){
+  localStorage.setItem("myList", tasks);
+  document.getElementById("myOL").innerHTML= "Saved Changes"
+}
+document.getElementById("button3").addEventListener("click",viewlist);
+function viewlist(){
+var d =  localStorage.getItem("myList");
+document.getElementById("myOL").innerHTML= d
 }
